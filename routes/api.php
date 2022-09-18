0fc760c2ce8c9baf412a80553dba942dc0b41e05
StenlySamberi <i.connect@outlook.co.idclear>
2022-09-18 18:25:06 +0700

    vendor

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::group(['middleware' => 'guest'],function(){
     Route::post('/materials',[MaterialController::class,'store'])->name('materials.post');
