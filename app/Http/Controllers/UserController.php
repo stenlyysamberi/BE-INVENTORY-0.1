e62a6cff8814485e59abcd3b9b2e47d0f4fe1014
StenlySamberi <i.connect@outlook.co.idclear>
2022-09-18 20:41:56 +0700

    add API Users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,6 @@ class UserController extends Controller
     public function users_all(){
         $user = User::all();
         return response()->json([
-            'status'   => 200,
-            'message'  => 'Data berhasil ditemukan',
             'data'     => $user
         ]);
     }
