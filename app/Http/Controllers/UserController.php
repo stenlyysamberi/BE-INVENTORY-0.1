291d6a86509c09ba7b2f8e4a9fb4371091543a37
StenlySamberi <i.connect@outlook.co.idclear>
2022-09-19 12:26:59 +0700

    add API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function users_all(){
 
-        $total = Material::select('qyt')->get()->sum();
+        $total = Material::select('SUM(qyt)')->get();
 
         $user = User::all()->all();
         return response()->json([
