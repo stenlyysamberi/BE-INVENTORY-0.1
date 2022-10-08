92fa7547f91fc90c516e6642e172241d7533f4d1
StenlySamberi <i.connect@outlook.co.idclear>
2022-10-08 07:26:49 +0700

    respon token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -72,7 +72,8 @@ class UserController extends Controller
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
-        return $this->respondWithToken($token);
+        //return $this->respondWithToken($token);
+        return "berhasil";
 
     }
 }
