2ae50befe7ca013cf17dedb4bf0d6eadbe89316c
StenlySamberi <i.connect@outlook.co.idclear>
2022-11-19 18:57:28 +0700

    okessss

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -49,7 +49,7 @@ class UserController extends Controller
 
         $user = User::create(
             [
-                'nama' => $request->input('nama'),
+                'nama' => $request->nama,
                 'company' => $request->input('company'),
                 'company_contact' => $request->input('contact_company'),
                 'email' => $request->input('email'),
